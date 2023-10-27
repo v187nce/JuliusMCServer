6798fec2721f576455a876a7f9440eead2039caa
v187nce <134378283+v187nce@users.noreply.github.com>
2023-10-27 06:39:49 +0000

    schwanz

---
+++ BackPack.java
@@ -0,0 +1,13 @@
+import org.bukkit.Bukkit;
+
+public class BackPack implements Listener{
+
+    private File file; 
+
+    public void onCloseInv(InventoryCloseEvent e){
+        Player p = e.getPlayer();
+        if (){
+
+        }
+    }
+}
---
+++ Main.java
@@ -0,0 +1,12 @@
+import org.bukkit.Bukkit; 
+
+public class Main implements JavaPlugin{
+
+    public void onEnable(){
+
+    }
+
+    public void onDisable(){
+        
+    }
+}
